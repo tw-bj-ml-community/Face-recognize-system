22e0a716f10351eff6b1f591cec89c459decf00a
hchan <hchan@thoughtworks.com>
2018-02-05 20:40:36 +0800

    add tolerance to face comparation

--- hchan/recognize101.py
+++ hchan/recognize101.py
@@ -62,7 +62,7 @@ while True:
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding,tolerance=0.4)
             name = "Unknown"
 
             # If a match was found in known_face_encodings, just use the first one.
