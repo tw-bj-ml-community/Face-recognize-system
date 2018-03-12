0b7850b8cc05a5dcf20a421252272e00dbbdb62a
hchan <hchan@thoughtworks.com>
2018-03-12 12:32:42 +0800

    [@hchan] modify the demo db and change instruction of db initialization.

--- hchan/fr_system/initial_database.sql
+++ hchan/fr_system/initial_database.sql
@@ -1,4 +1,4 @@
--- run this script by using "sqlite3 auction.db < create.sql" to create a brand new dev db
+-- run "sqlite3 dev.db < initial_database.sql" on "fr_system" folder to create a brand new dev db
 
 CREATE TABLE user_info (id INTEGER PRIMARY KEY, user_name TEXT NOT NULL);
 CREATE TABLE face_feature (id INTEGER PRIMARY KEY,user_id INTEGER , face_feature BLOB,FOREIGN KEY(user_id) REFERENCES user_info(id));
